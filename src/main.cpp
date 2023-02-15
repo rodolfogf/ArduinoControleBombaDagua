746749bd1e120429946be5c368d277966fe89f2c
Rodolfo Faria <rodolfogf@gmail.com>
2023-02-14 22:21:30 -0300

    Inclusão dos comentários e impressão do valor do pwm

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 /*
-erro = valor_ref - valor_nivel
-pwm = erro * kp
 comentada e função "desligar bomba" uma vez que com o erro tendendo a zero o valor 
 do pwm também tende a zero, ou seja, potência muito baixa ou nula
+os valores de kp, ki e kd foram os sugeridos pelo Marcos
 */
 
 #include <Arduino.h>
@@ -29,22 +28,31 @@ double erro_previo;
 double erro_atual;
 double erro_acumulado;
 
+//função que aciona a bomba com a potência de acordo com o valor de pwm (0 a 255)
 void ligarBomba(int valor_pwm)
 {
   analogWrite(pinBomba, valor_pwm);
 }
 
+//função que define o valor do pwm
 int definirPwm(double valor_nivel, double erro_somatorio, double erro_anterior)
 {  
   double erro_atual = valor_ref - valor_nivel;
+
+  //é uma aproximação da integral (arquivo "Integral.png")
   erro_somatorio = (erro_atual * (time_now - time_after) + erro_somatorio);
+
+  //é uma aproximação da derivada
   double derivada_aprox = (erro_atual - erro_anterior)/(time_now - time_after);
 
   int pwm = erro_atual * kp + (ki * erro_somatorio) + kd * derivada_aprox;
 
+  //o valor do pwm não pode ser menor que 0 nem maior que 255
   if (pwm > 255) pwm = 255;
   else if (pwm < 0) pwm = 0;
 
+  /*na próxima iteração o valor atual de tempo será o valor anterior de tempo
+  o mesmo raciocínio serve para o erro*/
   time_after = time_now;
   erro_anterior = erro_atual;
 
@@ -74,11 +82,13 @@ void loop()
   distancia = (vsom * pulseIn(pinSensorEcho, HIGH))/2;  
   nivel = altura - distancia;  
 
-  /*if (nivel < nivel_max) ligarBomba(definirPwm(nivel));
-  //else desligarBomba();*/
+  /*O valor do pwm é passado para a função "ligarBomba".
+  Esse valor é calculado na função "definirPwm, que apenas retorna o valor do pwm*/
+  double valor_pwm = definirPwm(nivel, erro_acumulado, erro_previo);
 
-  ligarBomba(definirPwm(nivel, erro_acumulado, erro_previo));
+  ligarBomba(valor_pwm);
 
+  //serve para imprimir o cabeçalho na primeira vez
   if (cabecalho == 1){
     Serial.print("tempo;nivel\n");
     cabecalho = 0;
@@ -87,6 +97,8 @@ void loop()
   Serial.print(time_now);
   Serial.print(";");
   Serial.println(nivel);
+  Serial.print(";");
+  Serial.print(valor_pwm);
   Serial.print("\n");
 
   delay(50);
