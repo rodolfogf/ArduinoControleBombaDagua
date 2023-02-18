46ec360c15fe4fa87a98be2db9bc7fd46385d071
Rodolfo Faria <rodolfogf@gmail.com>
2023-02-18 14:37:45 -0300

    Inclusão do atraso e alteração do tipo e nome das variáveis de tempo

--- leituras_backup2.py
+++ leituras_backup2.py
@@ -2,22 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 leituras = pd.read_csv(
-    r'C:\_UEMG\Disciplinas\Controle e Servomecanismo\Trabalho1_ControleBomba2\DadosIniciais.csv',
+    r'C:\_UEMG\Disciplinas\Controle e Servomecanismo\Trabalho1_ControleBomba2\CoolTerm Capture 2023-02-08 21-43-37.txt.csv',
     sep=';')
 pd.set_option('display.max_rows', None)
-
-#---------------------- Plot do gravação sem correções e normalização ------------------------#
-'''
-tempo = leituras['tempo']
-nivel = leituras['nivel']
-
-fig = plt.figure(figsize=(10, 7))
-plt.plot(tempo, nivel)
-plt.show()
-'''
-#---------------------------------------------------------------------------------------------#
-
-#---------------------------------- Correções e normalização ---------------------------------#
 
 leituras['nivel'] = leituras['nivel'].apply(lambda x: x - 2.09)
 leituras = leituras.drop(leituras[leituras.nivel < 0].index)
@@ -26,10 +13,26 @@ leituras = leituras.reset_index().drop(columns=['index'])
 tempo = leituras['tempo']
 nivel = leituras['nivel']
 
+'''
+q1 = leituras.groupby('tempo').quantile(0.25)
+q3 = leituras.groupby('tempo').quantile(0.75)
+iqr = q3 - q1
+li = q1 - 1.5 * iqr
+ls = q3 + 1.5 * iqr
+li = li['nivel']
+ls = ls['nivel']
+'''
+
 # maximos = leituras.groupby('tempo').max()
 # minimos = leituras.groupby('tempo').min()
 medias = leituras.groupby('tempo').mean()['nivel']
 desvpad = leituras.groupby('tempo').std()['nivel']
+
+# teste = li['nivel'][0]
+
+# leituras = leituras.drop(leituras[leituras.nivel < 0].index)
+# teste = li.query('tempo == 70')
+# teste2 = li.loc[70]
 
 leituras_corrigidas = leituras
 
@@ -48,7 +51,12 @@ medias_corrigido = leituras.groupby('tempo').mean()['nivel']
 
 tempo_list = list(set(leituras_corrigidas['tempo'].tolist()))
 
+# print(leituras)
+print(leituras_corrigidas)
+# print(leituras_corrigidas.iloc[629])
+
 fig = plt.figure(figsize=(10, 7))
+# plt.plot(tempo, nivel)
+# plt.plot(tempo_corrigido, nivel_corrigido)
 plt.plot(tempo_list, medias_corrigido)
 plt.show()
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 /*
-comentada e função "desligar bomba" uma vez que com o erro tendendo a zero o valor 
-do pwm também tende a zero, ou seja, potência muito baixa ou nula
 os valores de kp, ki e kd foram os sugeridos pelo Marcos
 */
 
@@ -9,23 +7,24 @@ os valores de kp, ki e kd foram os sugeridos pelo Marcos
 # define pinSensorTrig 8
 # define pinSensorEcho 7
 # define pinBomba 11
-# define altura 23.6
+# define altura 24.00
 # define vsom 0.0343
 # define nivel_max 15.00
 # define kp 80.00
 # define ki 0.1
 # define kd 35.00
 
-bool atraso = true;
 bool cabecalho;
-float time_now;
-float time_after;
+unsigned long tempo_agora;
+unsigned long tempo_antes;
+unsigned long tempo_segundos;
 double distancia;
 double nivel;
 double valor_ref;
 double erro_previo;
 double erro_atual;
 double erro_acumulado;
+unsigned long  atraso_inicio;
 
 //função que aciona a bomba com a potência de acordo com o valor de pwm (0 a 255)
 void ligarBomba(int valor_pwm)
@@ -39,10 +38,10 @@ int definirPwm(double valor_nivel, double erro_somatorio, double erro_anterior)
   double erro_atual = valor_ref - valor_nivel;
 
   //é uma aproximação da integral (arquivo "Integral.png")
-  erro_somatorio = (erro_atual * (time_now - time_after) + erro_somatorio);
+  erro_somatorio = erro_atual * (tempo_agora - tempo_antes)/1000.00 + erro_somatorio;
 
   //é uma aproximação da derivada
-  double derivada_aprox = (erro_atual - erro_anterior)/(time_now - time_after);
+  double derivada_aprox = (erro_atual - erro_anterior)/(tempo_agora - tempo_antes)/1000.00;
 
   int pwm = erro_atual * kp + (ki * erro_somatorio) + kd * derivada_aprox;
 
@@ -52,7 +51,7 @@ int definirPwm(double valor_nivel, double erro_somatorio, double erro_anterior)
 
   /*na próxima iteração o valor atual de tempo será o valor anterior de tempo
   o mesmo raciocínio serve para o erro*/
-  time_after = time_now;
+  tempo_antes = tempo_agora;
   erro_anterior = erro_atual;
 
   return pwm;
@@ -66,40 +65,45 @@ void setup()
   cabecalho = 1;
   valor_ref = nivel_max;
   Serial.begin(9600);
-  time_after = 0;
+  tempo_antes = 0;
   erro_previo = 0;
+  atraso_inicio = 10000;
 }
 
 void loop()
 {  
-  time_now = millis() / 1000;
+  tempo_agora = millis();
+  tempo_segundos = tempo_agora/1000;
 
-  digitalWrite(pinSensorTrig, HIGH);
-  delayMicroseconds(15);
-  digitalWrite(pinSensorTrig, LOW);
+  if (tempo_agora > atraso_inicio){
 
-  distancia = (vsom * pulseIn(pinSensorEcho, HIGH))/2;  
-  nivel = altura - distancia;  
+    digitalWrite(pinSensorTrig, HIGH);
+    delayMicroseconds(15);
+    digitalWrite(pinSensorTrig, LOW);
 
-  /*O valor do pwm é passado para a função "ligarBomba".
-  Esse valor é calculado na função "definirPwm, que apenas retorna o valor do pwm*/
-  double valor_pwm = definirPwm(nivel, erro_acumulado, erro_previo);
+    distancia = (vsom * pulseIn(pinSensorEcho, HIGH))/2;  
+    nivel = altura - distancia;  
 
-  ligarBomba(valor_pwm);
+    /*O valor do pwm é passado para a função "ligarBomba".
+    Esse valor é calculado na função "definirPwm, que apenas retorna o valor do pwm*/
+    double valor_pwm = definirPwm(nivel, erro_acumulado, erro_previo);
 
-  //serve para imprimir o cabeçalho na primeira vez
-  if (cabecalho == 1){
-    Serial.print("tempo;nivel\n");
-    cabecalho = 0;
-  }
-  
-  Serial.print(time_now);
-  Serial.print(";");
-  Serial.println(nivel);
-  Serial.print(";");
-  Serial.print(valor_pwm);
-  Serial.print("\n");
+    ligarBomba(valor_pwm);
 
-  delay(50);
+    //serve para imprimir o cabeçalho na primeira vez
+    if (cabecalho == 1){
+      Serial.print("tempo;nivel;pwm\n");
+      cabecalho = 0;
+    }
+
+    Serial.print(tempo_segundos);
+    Serial.print(";");
+    Serial.print(nivel);
+    Serial.print(";");
+    Serial.print(valor_pwm);
+    Serial.print("\n");
+
+    //Utilizado para corrigir os valores discrepantes que estavam sendo impressos no gráfico
+    delay(50);
+  }  
 }
-
